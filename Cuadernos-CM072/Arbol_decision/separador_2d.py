3619c9da59124c531ea342142c4b437d8ef13c28
C-Lara <claraa@uni.edu.pe>
2018-09-23 01:35:52 -0500

    script 2

---
+++ Cuadernos-CM072/Arbol_decision/separador_2d.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+cm2 = ListedColormap(['#0000aa', '#ff2020'])
+
+cdict = {'red': [(0.0, 0.0, cm2(0)[0]),
+                 (1.0, cm2(1)[0], 1.0)],
+
+         'green': [(0.0, 0.0, cm2(0)[1]),
+                   (1.0, cm2(1)[1], 1.0)],
+
+         'blue': [(0.0, 0.0, cm2(0)[2]),
+                    (1.0, cm2(1)[2], 1.0)]}
+ReBl = LinearSegmentedColormap("ReBl", cdict)
+
+import numpy as np
+cm3 = ListedColormap(['#0000aa', '#ff2020', '#50ff50'])
+
+def dibuja_separador_2d(clasificador, X, relleno=False, ax=None, eps=None, alfa=1,
+                      cm=cm2, ancholinea=None, umbral=None,
+                      estilolinea="solid"):
+
+    if eps is None:
+        eps = X.std() / 2.
+
+    if ax is None:
+        ax = plt.gca()
+
+    x_min, x_max = X[:, 0].min() - eps, X[:, 0].max() + eps
+    y_min, y_max = X[:, 1].min() - eps, X[:, 1].max() + eps
+    xx = np.linspace(x_min, x_max, 1000)
+    yy = np.linspace(y_min, y_max, 1000)
+
+    X1, X2 = np.meshgrid(xx, yy)
+    X_grid = np.c_[X1.ravel(), X2.ravel()]
+    try:
+        valor_decision = clasificador.decision_function(X_grid)
+        niveles = [0] if umbral is None else [umbral]
+        niveles_f = [valor_decision.min()] + niveles + [valor_decision.max()]
+    except AttributeError:
+        # No funciones de decision
+        valor_decision = clasificador.predict_proba(X_grid)[:, 1]
+        niveles = [.5] if umbral is None else [umbral]
+        niveles_f = [0] + niveles + [1]
+    if relleno:
+        ax.contourf(X1, X2, valor_decision.reshape(X1.shape),
+                    levels=niveles_f, alpha=alfa, cmap=cm)
+    else:
+        ax.contour(X1, X2, valor_decision.reshape(X1.shape), levels=niveles,
+                   colors="black", alpha=alfa, linewidths=ancholinea,
+                   linestyles=estilolinea, zorder=5)
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
