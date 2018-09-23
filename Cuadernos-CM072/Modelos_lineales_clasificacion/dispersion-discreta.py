fd72ba3f5b538092b189c990692b8d854371a823
C-Lara <claraa@uni.edu.pe>
2018-09-22 20:59:44 -0500

    script 1

---
+++ Cuadernos-CM072/Modelos_lineales_clasificacion/dispersion-discreta.py
@@ -0,0 +1,60 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.colors import  colorConverter
+import numpy as np
+
+def dibuja_dispersion_discreta(x1, x2, y=None, marcadores=None, s=10, ax=None,
+                     etiquetas=None, relleno=.2, alfa=1, c=None, anchobordemarcador=None):
+    """Adaptacion de la funcion matplotlib.pyplot.scatter a dibujar clases o  clusters"""
+    
+    if ax is None:
+        ax = plt.gca()
+
+    if y is None:
+        y = np.zeros(len(x1))
+
+    unico_y = np.unique(y)
+
+    if marcadores is None:
+        marcadores= ['o', '^', 'v', 'D', 's', '*', 'p', 'h', 'H', '8', '<', '>'] * 10
+
+    if len(marcadores) == 1:
+        marcadores = marcadores * len(unico_y)
+
+    if etiquetas is None:
+        etiquetas = unico_y
+
+    # lineas en el sentido de matplotlib
+    lineas = []
+
+    actual_cycler = mpl.rcParams['axes.prop_cycle']
+
+    for i, (yy, cycle) in enumerate(zip(unico_y, actual_cycler())):
+        mascara = y == yy
+        # if c is ninguno, use color cycle
+        if c is None:
+            color = cycle['color']
+        elif len(c) > 1:
+            color = c[i]
+        else:
+            color = c
+        # bordes claros para marcadores oscuros
+        if np.mean(colorConverter.to_rgb(color)) < .4:
+            colorbordemarcador = "grey"
+        else:
+            colorbordemarcador = "black"
+
+        lineas.append(ax.plot(x1[mascara], x2[mascara], marcadores[i], markersize=s,
+                             label=etiquetas[i], alpha=alfa, c=color,
+                             markeredgewidth=anchobordemarcador,
+                             markeredgecolor=colorbordemarcador)[0])
+
+    if relleno != 0:
+        pad1 = x1.std() * relleno
+        pad2 = x2.std() * relleno
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        ax.set_xlim(min(x1.min() - pad1, xlim[0]), max(x1.max() + pad1, xlim[1]))
+        ax.set_ylim(min(x2.min() - pad2, ylim[0]), max(x2.max() + pad2, ylim[1]))
+
+    return lineas
